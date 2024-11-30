9407af634ec0c48b4f6952dafcbc89e523c164cc
Gitty <alliecurrie@msn.com>
2024-11-30 15:28:52 -0500

    My Repository

---
+++ src/main/java/net/carter/theenderadventure/TheEnderAdventure.java
@@ -0,0 +1,16 @@
+package net.carter.theenderadventure;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TheEnderAdventure implements ModInitializer {
+	public static final String MOD_ID = "the-ender-adventure";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/carter/theenderadventure/TheEnderAdventureClient.java
@@ -0,0 +1,10 @@
+package net.carter.theenderadventure;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheEnderAdventureClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
